56cc060a96eba615be297ae53d2250a8b0c579ce
Rubens Victor <rubensvictorsantos@gmail.com>
2020-11-24 16:22:28 -0300

    Parte de recolher as opções

--- modules.js
+++ modules.js
@@ -1,14 +1,14 @@
-    const painel = document.getElementById('painel');
+    const painel    = document.getElementById('painel');
 
-    const btnPath = document.getElementById('btn-path');
-    const btnRect = document.getElementById('btn-rect');
+    const btnPath   = document.getElementById('btn-path');
+    const btnRect   = document.getElementById('btn-rect');
     const btnCircle = document.getElementById('btn-circle');
-    const btnLine = document.getElementById('btn-line');
-    const plcap = document.getElementById('p-str-lcap');
-    const pljoin = document.getElementById('p-str-ljoin');
+    const btnLine   = document.getElementById('btn-line');
+    const plcap     = document.getElementById('p-str-lcap');
+    const pljoin    = document.getElementById('p-str-ljoin');
     const boxPainel = document.getElementById('container');
-    const boxcode = document.getElementById('box-code');
-    const textA = document.getElementById('textA');
+    const boxcode   = document.getElementById('box-code');
+    const textA     = document.getElementById('textA');
 
     function openBoxPainel(e){
 
@@ -18,11 +18,11 @@
 
         if(box.style.display == 'none'){
             box.style.display = 'block';
-            btn.value = ' -';
+            btn.innerHTML = '&#9652;';
 
         }else{
             box.style.display = 'none';
-            btn.value = '+';
+            btn.innerHTML = '&#9662;' ;
 
         }
     }    
@@ -198,7 +198,6 @@
     function bgBody(e){
 
         document.querySelectorAll(".box").forEach((box) => {
-            // viewCode.classList.add('open-viewcode');
 
             box.addEventListener('click', (event) => {             
                 boxcode.append(viewCode)
@@ -206,7 +205,6 @@
                 
             });
         });
-
     }
 
     painel.addEventListener('input', svg, false);
